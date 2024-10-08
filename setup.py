47926bf1a7fc4408b8e3a0f591318bbe4b525358
yanghongchhy <2650502478@qq.com>
2024-10-09 00:20:32 +0800

    first commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_namespace_packages
 from os import path
 
 DIR = path.dirname(path.abspath(__file__))
-INSTALL_PACKAGES = open(path.join(DIR,  'requirements.txt')).read().splitlines()
+INSTALL_PACKAGES = open(path.join(DIR,  'requirements.txt'), encoding='utf-8').read().splitlines()
 
 setup(
     name="PhaseMotif",
@@ -12,3 +12,4 @@ setup(
     install_requires=INSTALL_PACKAGES,
     python_requires='>=3.8',
 )
+# pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 --index-url https://download.pytorch.org/whl/cu121
