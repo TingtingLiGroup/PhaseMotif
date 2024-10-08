3a8693ca97c8782b8395fb9dfb43826866224528
yanghongchhy <2650502478@qq.com>
2024-10-08 12:49:29 +0800

    first commit

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ setup(
     name="PhaseMotif",
     version="0.1",
     packages=find_namespace_packages(),
+    include_package_data=True,
     install_requires=[
         'torch',
         'torchvision',
