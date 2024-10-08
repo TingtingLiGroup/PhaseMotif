83d19889a04feeffede2e2197c1b366fecb95b9b
yanghongchhy <2650502478@qq.com>
2024-10-08 23:31:56 +0800

    first commit

--- setup.py
+++ setup.py
@@ -1,21 +1,14 @@
 from setuptools import setup, find_namespace_packages
+from os import path
+
+DIR = path.dirname(path.abspath(__file__))
+INSTALL_PACKAGES = open(path.join(DIR,  'requirements.txt')).read().splitlines()
+
 setup(
     name="PhaseMotif",
     version="0.1",
     packages=find_namespace_packages(),
     include_package_data=True,
-    install_requires=[
-        'torch>=2.0',
-        'torchvision',
-        'pandas',
-        'numpy',
-        'scikit-learn',
-        'umap-learn',
-        'matplotlib',
-        'seaborn',
-        'scipy=1.10',
-        'joblib',
-    ],
+    install_requires=INSTALL_PACKAGES,
     python_requires='>=3.8',
-
 )
